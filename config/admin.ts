e392c6822e6275433caf80a184b100db26224907
Rayen Ben Hassine <rayenbenhassindjo@gmail.com>
2025-05-12 18:41:37 +0100

    installing on windows

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'VSkB4vfEinqPZiv0INg+Mg=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'XhKoURBByVU+dDSUQd8u7Q=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', '9T2XWzxvmxH8g2czOuiaoA=='),
     },
   },
   flags: {
